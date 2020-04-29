21122ab73d7cde1ab7619694cb71f25707e1bfcb
Greccu <56735903+Greccu@users.noreply.github.com>
2020-04-29 12:17:55 +0300

    Add files via upload

--- skiplist.py
+++ skiplist.py
@@ -28,8 +28,6 @@ class Node:
 
 
 class SkipList:
-    # primul nod este -1 iar ultimul +inf
-    # primul si ultimul nod se afla pe mereu nivelul maxim
     def __init__(self):
         self.startnode = Node(-1, 0, None, [])
         self.endnode = Node(float("inf"), 0, self.startnode, None)
@@ -49,20 +47,17 @@ class SkipList:
                     currentnode = currentnode.successors[i]
 
             print()
-        print("stop afisare")
-        print()
+
     def search(self, value):
         level = self.startnode.level
         currentnode = self.startnode
         while level >= 0:
             if currentnode.successors[level].value < value:
                 currentnode = currentnode.successors[level]
-                #print("Nivel curent {}, nod curent{}".format(level,currentnode.value))
             elif currentnode.successors[level].value == value:
                 return 1
             else:
                 level -= 1
-        #print("cautare",value,level,currentnode.value)
         return 0
 
 
@@ -70,19 +65,14 @@ class SkipList:
         level = self.startnode.level
         currentnode = self.startnode
         while level>=0:
-            #print("nivel",level,"nod",currentnode.value,"valoare",value)
             if currentnode.successors[level].value < value:
-                #print("caz 1")
                 currentnode = currentnode.successors[level]
             elif currentnode.successors[level].value > value:
-                #print("caz 2")
                 level-=1
             else:
-                #print("caz 3")
                 currentnode=currentnode.successors[level]
                 break
 
-        #print("inserare",value,"gasit predecesor",currentnode.value)
         level = 0
         aux=currentnode.successors[level]
         currentnode.successors[level] = Node(value, 0, currentnode, aux)
@@ -92,7 +82,6 @@ class SkipList:
             level+=1
             currentnode.level+=1
             if level > self.startnode.level:
-                #print("nivel inexistent")
                 self.startnode.level+=1
                 self.startnode.successors.append(currentnode)
                 self.endnode.level+=1
@@ -100,14 +89,9 @@ class SkipList:
                 currentnode.predecessors.append(self.startnode)
                 currentnode.successors.append(self.endnode)
             else:
-                #print("nivel existent")
                 predecessor = currentnode.predecessors[level-1]
-                #print("nod curent",currentnode.value,"predecesor",predecessor.value,"nivel",level)
                 while predecessor.level<level:
-                    #print("nod curent", currentnode.value, "predecesor", predecessor.value, "nivel", level)
-                    #print(predecessor.predecessors,predecessor.successors)
                     predecessor = predecessor.predecessors[level-1]
-                #("gasit primul predecesor cu nivel indeajuns de mare",predecessor.value)
                 successor=predecessor.successors[level]
                 predecessor.successors[level]=currentnode
                 currentnode.predecessors.append(predecessor)
@@ -115,18 +99,99 @@ class SkipList:
                 successor.predecessors[level]=currentnode
 
 
+    def delete(self,value):
+        level = self.startnode.level
+        currentnode = self.startnode
+        while level >= 0:
+            if currentnode.successors[level].value < value:
+                currentnode = currentnode.successors[level]
+            elif currentnode.successors[level].value > value:
+                level -= 1
+            else:
+                currentnode = currentnode.successors[level]
+                break
+        nr=0
+        while currentnode.value==value:
+            while currentnode.level>=0:
+                currentnode.predecessors[currentnode.level].successors[currentnode.level]=currentnode.successors[currentnode.level]
+                currentnode.successors[currentnode.level].predecessors[currentnode.level]=currentnode.predecessors[currentnode.level]
+                currentnode.level-=1
+            if currentnode.predecessors[0].value==value:
+                currentnode=currentnode.predecessors[0]
+            else:
+                currentnode=currentnode.successors[0]
+            nr+=1
+        return nr
+
+    def predecessor(self,value):
+        level = self.startnode.level
+        currentnode = self.startnode
+        while level >= 0:
+            if currentnode.successors[level].value < value:
+                currentnode = currentnode.successors[level]
+            elif currentnode.successors[level].value > value:
+                level -= 1
+            else:
+                currentnode = currentnode.successors[level]
+                break
+        return currentnode.value
+
+    def successor(self,value):
+        level = self.startnode.level
+        currentnode = self.startnode
+        while level >= 0:
+            if currentnode.successors[level].value < value:
+                currentnode = currentnode.successors[level]
+            elif currentnode.successors[level].value > value:
+                level -= 1
+            else:
+                currentnode = currentnode.successors[level]
+                break
+        if currentnode.value<value:
+            currentnode=currentnode.successors[0]
+        if currentnode.value==float("inf"):
+            return -1
+        return currentnode.value
+
+    def interval(self,value,value2):
+        level = self.startnode.level
+        currentnode = self.startnode
+        while level >= 0:
+            if currentnode.successors[level].value < value:
+                currentnode = currentnode.successors[level]
+            elif currentnode.successors[level].value > value:
+                level -= 1
+            else:
+                currentnode = currentnode.successors[level]
+                break
+        while currentnode.value <= value:
+            currentnode = currentnode.successors[0]
+        inter=""
+        while currentnode.value < value2:
+            inter = inter + str(currentnode.value) + " "
+            currentnode=currentnode.successors[0]
+        return inter[:-1]
 
 
-l = SkipList()
-l.insert(7)
-#l.afisare()
-l.insert(3)
-#l.afisare()
-l.insert(8)
-#l.afisare()
-l.insert(9)
-#l.afisare()
-l.insert(5)
-#l.afisare()
-l.afisare()
-print(l.search(7))
+switcher = {
+    "1":SkipList.insert,
+    "2":SkipList.delete,
+    "3":SkipList.search,
+    "4":SkipList.predecessor,
+    "5":SkipList.successor,
+    "6":SkipList.interval
+}
+
+with open("abce.in",'r') as f:
+    with open("abce.out",'w') as g:
+        l = SkipList()
+        n=int(f.readline())
+        for _ in range(n):
+            x=f.readline().split()
+            if x[0] in ("1","2"):
+                switcher[x[0]](l,int(x[1]))
+            elif x[0] == "6":
+                g.write(switcher[x[0]](l,int(x[1]),int(x[2]))+"\n")
+
+            else:
+                g.write(str(switcher[x[0]](l,int(x[1])))+"\n")
